eaf25804e14ff52e0ebe45c8424b314acc78d29d
Andrew Johnson <7144201+acjsec@users.noreply.github.com>
2025-11-23 07:03:27 -0800

    Summarize titles with OpenAI

--- reddit/reddit.py
+++ reddit/reddit.py
@@ -2,9 +2,9 @@ from aws_get_secret import get_secret
 import json
 import argparse
 import requests
+from openai import OpenAI
 
 parser = argparse.ArgumentParser(description="Summarize Reddit posts using OpenAI")
-
 parser.add_argument("-r", "--subreddit", 
                     default="popular", 
                     help="Subreddit")
@@ -17,7 +17,6 @@ parser.add_argument("-t", "--top",
                     default="day", 
                     choices=["hour", "day", "week", "month", "year", "all"],
                     help="Sort method")
-
 args = parser.parse_args()
 
 try:
@@ -26,6 +25,8 @@ except Exception as e:
     print(e)
 
 openai_api_key = json.loads(secret)['OpenAI-Test']
+
+client = OpenAI(api_key=openai_api_key)
 
 subreddit = args.subreddit
 sort = args.sort
@@ -36,8 +37,7 @@ if sort == "top":
    url += f"?t={top}"
 
 headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"}
-
-response = requests.get(url, headers=headers)
+web_response = requests.get(url, headers=headers)
 
 def find_all_titles(obj):
   titles = []
@@ -54,10 +54,17 @@ def find_all_titles(obj):
 
   return titles
 
+all_titles = find_all_titles(web_response.json())
 
-all_titles = find_all_titles(response.json())
+openai_response = client.chat.completions.create(
+    model="gpt-5.1",
+    messages=[
+        {"role": "user", "content": f"Summarize the following Reddit post titles in a concise manner. Summarize in paragraph form and do not simply provide a list:\n\n{all_titles}"}
+    ]
+)
 
-print(all_titles)
-print(args)
-print(url)
+print(openai_response.choices[0].message.content)
 
+#print(all_titles)
+#print(args)
+#print(url)
