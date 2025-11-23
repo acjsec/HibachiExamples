a8ff0d507ca525f11092a0b0cb3bae009f81b86b
Andrew Johnson <7144201+acjsec@users.noreply.github.com>
2025-11-22 16:51:05 -0800

    Implement argparser

--- reddit/reddit.py
+++ reddit/reddit.py
@@ -1,5 +1,24 @@
 from aws_get_secret import get_secret
 import json
+import argparse
+
+parser = argparse.ArgumentParser(description="Summarize Reddit posts using OpenAI")
+
+parser.add_argument("-r", "--subreddit", 
+                    default="popular", 
+                    help="Subreddit")
+parser.add_argument("-s", "--sort", 
+                    default="top", 
+                    choices=["best", "hot", "new", "top", "rising"],
+                    required=True,
+                    help="Sort method")
+parser.add_argument("-t", "--top", 
+                    default="day", 
+                    choices=["hour", "day", "week", "month", "year", "all"],
+                    help="Sort method")
+
+args = parser.parse_args()
+
 
 try:
   secret = get_secret()
@@ -7,4 +26,15 @@ except Exception as e:
     print(e)
 
 openai_api_key = json.loads(secret)['OpenAI-Test']
+
+subreddit = args.subreddit
+sort = args.sort
+top = args.top
+url = f"https://www.reddit.com/r/{subreddit}/{sort}" 
+#if sort == "top":
+#    url += f"?t={top}"
+
+print(args)
+
+print(url)
 
